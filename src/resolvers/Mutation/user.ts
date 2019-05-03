9fe2dd24ffa39ddb6c1372a35d92ac7fd8cf52e2
VirtualSalesman <Mat.C.Yates@gmail.com>
2019-05-03 16:49:45 -0400

    It is now possible to create a user

--- src/resolvers/Mutation/user.ts
+++ src/resolvers/Mutation/user.ts
@@ -1,4 +1,6 @@
 import bcrypt from 'bcryptjs';
+import generateToken  from '../../utils/generateToken';
+import getUserId from '../../utils/getUserId';
 
 export const user = {
   async createUser(
@@ -6,6 +8,27 @@ export const user = {
       name: string, email: string, password: string
     }}, {prisma}: any, info: any
   ) {
+    const data = args.data;
 
+    const emailTaken = await prisma.exists.User({ email: data.email});
+    if (emailTaken) {
+      throw new Error('Email taken')
+    }
+    if (data.password.length < 8) {
+      throw new Error('Password must be atleast 8 characters or longer')
+    }
+
+    const password = await bcrypt.hash(data.password, 10);
+
+    const user = await prisma.mutation.createUser({
+      data: {
+        ...data,
+        password
+      }
+    }).catch((e: void) => console.error(e));
+    return {
+      user,
+      token: generateToken(user.id)
+    };
   },
 }
