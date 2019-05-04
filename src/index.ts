dd1d8a34f84402e41e88b006f69f8bb011ae83bd
VirtualSalesman <Mat.C.Yates@gmail.com>
2019-05-04 14:31:53 -0400

    .

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import { GraphQLServer, PubSub } from "graphql-yoga";
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
 import prisma from "./prisma";
 import { resolvers } from "./resolvers/index";
 const pubsub = new PubSub();
@@ -13,6 +15,17 @@ const server: GraphQLServer = new GraphQLServer({
 		};
 	}
 });
+const limiter = new rateLimit({
+	windowMs: 15 * 60 * 1000,
+	max: 200
+});
+
+server.express.use(helmet());
+// only if you're behind a reverse proxy
+// (Heroku, Bluemix, AWS if you use an ELB, custom Nginx setup, etc)
+server.express.enable("trust proxy");
+
+server.express.use(limiter);
 
 server.start(() => {
 	console.log("Started");
