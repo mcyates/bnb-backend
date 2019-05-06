54246412e0c1a8f241e7b24cee0c2a0fcdef0978
VirtualSalesman <Mat.C.Yates@gmail.com>
2019-05-06 14:25:43 -0400

    added a query fir a users bookings

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,15 +1,17 @@
+import { booking } from "./Mutation/booking";
 import { IResolvers } from "graphql-tools";
 import Query from "./Query";
-import { listing } from './Mutation/listing';
+import { listing } from "./Mutation/listing";
 import { user } from "./Mutation/user";
 import { review } from "./Mutation/review";
 
 const resolvers: IResolvers = {
 	Query,
 	Mutation: {
-    ...listing,
-    ...user,
-    ...review
+		...booking,
+		...listing,
+		...review,
+		...user
 	}
 };
 
