fd000f7678b23a645b07896ee0146eec4d0bf6a3
m-c-yates <mat.c.yates@gmail.com>
2019-08-05 14:12:20 +0000

    .

--- src/index.ts
+++ src/index.ts
@@ -7,29 +7,29 @@ import { resolvers } from "./resolvers/index";
 
 const pubsub = new PubSub();
 
-const server: GraphQLServer = new GraphQLServer({
+const server: GraphQLServer = new GraphQLServer( {
 	typeDefs: "./src/schema/schema.graphql",
 	resolvers,
-	context(request) {
+	context ( request ) {
 		return {
 			prisma,
 			request
 		};
 	}
-});
-const limiter = new rateLimit({
+} );
+const limiter = new rateLimit( {
 	windowMs: 60 * 1000,
 	max: 250
-});
+} );
 
-server.express.use(cors())
-server.express.use(helmet());
+server.express.use( cors() )
+server.express.use( helmet() );
 // only if you're behind a reverse proxy
 // (Heroku, Bluemix, AWS if you use an ELB, custom Nginx setup, etc)
-server.express.enable("trust proxy");
+server.express.enable( "trust proxy" );
 
-server.express.use(limiter);
+server.express.use( limiter );
 
-server.start(() => {
-	console.log("Started");
-});
+server.start( { port: 5000 }, () => {
+	console.log( "Started" );
+} );
